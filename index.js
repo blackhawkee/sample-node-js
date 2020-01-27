e0576b9dd53c4b5de7870709fad40d41e8487563
blackhawkee <premson.kp@gmail.com>
2020-01-27 22:42:25 +0400

    commit 5
    commit 5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! - Commit -4')
+  response.send('Hello World! - Commit -5')
 })
 
 app.listen(app.get('port'), function() {
